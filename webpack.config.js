c6b70f09e3f0e55a2d1514ce352f0260d0d3d80f
Stayko Chalakov <chalakov88@gmail.com>
2017-07-06 22:43:05 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  }
+}
